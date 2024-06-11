ffcfc2ee251d5124048199f26bb2e4298c45faa3
DTG-J <grozdanovdt@abv.bg>
2024-06-11 18:00:19 +0300

    Profile function in UserController

--- src/main/java/bg/softuni/pathfinder/web/UserController.java
+++ src/main/java/bg/softuni/pathfinder/web/UserController.java
@@ -70,6 +70,8 @@ private final UserService userService;
     }
     @GetMapping("users/profile")
     public ModelAndView profile(){
-        return new ModelAndView ("profile");
+        ModelAndView modelAndView = new ModelAndView ("profile");
+        modelAndView.addObject ("profileData", userService.getProfileData ());
+        return modelAndView;
     }
 }
